0b7daa7cad0a94ec2ce8ac72c0468f52fdbacb18
Sharma <hsharm16@LAMU02PJ3CDG3QC.uhc.com>
2015-08-05 13:32:03 +0530

    Add user

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803122634) do
+ActiveRecord::Schema.define(version: 20150805075257) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.integer  "user_id",    limit: 4
     t.integer  "counts",     limit: 4
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "comments", force: :cascade do |t|
